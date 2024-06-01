aa3c684a3558bdd37c6d2f96284eef71bab52f42
Sumithrajesh <142719216+Sumithrajesh@users.noreply.github.com>
2024-06-01 17:43:56 +0530

    Add files via upload

---
+++ frontend projects/dental clinic/dental clinic/script.js
@@ -0,0 +1,12 @@
+let mynavbar = document.querySelector('.navbar');
+let mybars = document.querySelector('#bar');
+mybars.onclick = () =>{
+    mybars.classList.toggle('fa-times')
+    mybars.classList.toggle('active')
+}
+// let menu = document.querySelector('#mbl);
+// // let nav = document.querySelector('nav);
+// // menu.onclick = () =>{
+// //     menu.classList.toggle('fa-times)    
+// //     nav.classList.toggle('active)    
+// // }`
